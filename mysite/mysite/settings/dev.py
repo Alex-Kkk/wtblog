41cc53a89d7b564ee7fbad87fe6b6a39cdf9d1f5
Alex K. <h_xx@mail.ru>
2025-11-03 16:31:17 +1100

    sync

---
+++ mysite/mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-*a=7ko9_+b0g69m%v4z46@47ym*5bs4)-rvc+a4r3s43jef6tm"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
